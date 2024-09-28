f92cde74c1dde9d7a3c45f8921bfe2569b78981e
czllll <czl010106@163.com>
2024-09-28 19:48:20 +0100

    feat:1.add footer;2. add sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +15,9 @@ export default defineConfig({
 }), mdx(), react()],
   redirects: {
     '/blog': '/blog/1'
-  }
+  },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });

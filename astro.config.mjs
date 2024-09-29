6407d1fb60917074380882c4cef81271e1aaa0b0
czllll <czl010106@163.com>
2024-09-29 18:13:57 +0100

    feat: add search tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
+import pagefind from "astro-pagefind";
 
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
-}), mdx(), react()],
+}), mdx(), react(),pagefind()],
   redirects: {
     '/blog': '/blog/1'
   },

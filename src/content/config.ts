45233b3d16599534c963f34d3388b7f7ad0d6a45
czllll <czl010106@163.com>
2024-10-30 21:03:37 +0000

    fix(build error): fix build error

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,7 @@ import { defineCollection, z } from "astro:content";
 const blogCollection = defineCollection({
     schema: ({ image }) => z.object({
         date: z.string(),
-        image: z.string(),
         title: z.string(),
-        description: z.string(),
         tags: z.array(z.string()).default([])
     })
 })

a8359d05d5bd755587b18f3daf1386e22e385999
czllll <czl010106@163.com>
2024-11-18 19:41:40 +0000

    fix: rss format

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
-import { marked } from 'marked';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context: { site: any; }) {
     const posts = await getCollection("posts");
@@ -11,12 +12,14 @@ export async function GET(context: { site: any; }) {
     site: context.site,
     items: await Promise.all(
         posts.map(async (post) => {
-          const content = await post.body;
+          const { Content } = await post.render();
           return {
             title: post.data.title,
             pubDate: post.data.date,
             description: post.data.description,
-            content: content || post.data.description,
+            content: sanitizeHtml(parser.render(post.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+              }),
             link: `/blog/${post.slug}/`,
           };
         })
